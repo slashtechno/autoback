bc5112a6116e61341c57e89fe2d33bb1bed4f9d4
slashtechno <77907286+slashtechno@users.noreply.github.com>
2026-02-19 18:55:06 -0600

    fix: don't set min height in route to screen as that messes up footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
   const session = authClient.useSession();
 </script>
 
-<div class="flex flex-col justify-center items-center gap-4 min-h-screen">
+<div class="flex flex-col justify-center items-center gap-4">
 {#if $session.data}
     <ul>
         <li><a href="/drives">Drives</a></li>
